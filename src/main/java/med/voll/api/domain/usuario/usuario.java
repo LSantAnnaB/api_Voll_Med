6bcae75aaaadac1984140aea159806f05d0c116b
Lucas Sant'Anna <98597613+LSantAnnaB@users.noreply.github.com>
2023-09-06 10:43:00 -0300

    adicionando service e repository de usuario

--- src/main/java/med/voll/api/domain/usuario/usuario.java
+++ src/main/java/med/voll/api/domain/usuario/usuario.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class usuario {
+public class Usuario {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
