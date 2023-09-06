3aa2f1bdafce5cc07ba6ef51632f8de49c999a46
Lucas Sant'Anna <98597613+LSantAnnaB@users.noreply.github.com>
2023-09-06 16:18:18 -0300

    interface usuario; bcrypt metodo

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class AutenticacaoController {
   @Autowired
   private AuthenticationManager manager;
 
+  @PostMapping
   public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
     var tokken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
     var autentication = manager.authenticate(tokken);
