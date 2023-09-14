22e5f5c969123e7d194910761cb581fa97f5cb9f
Lucas Sant'Anna <98597613+LSantAnnaB@users.noreply.github.com>
2023-09-14 16:24:14 -0300

    DTO e Controller de paciente

--- src/main/java/med/voll/api/domain/paciente/Paciente.java
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -42,4 +42,20 @@ public class Paciente {
     this.endereco = new Endereco(dados.endereco());
   }
 
+  public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+    if (dados.nome() != null) {
+      this.nome = dados.nome();
+    }
+    if (dados.telefone() != null) {
+      this.telefone = dados.telefone();
+    }
+    if (dados.endereco() != null) {
+      this.endereco.atualizarInformacoes(dados.endereco());
+    }
+
+  }
+
+  public void excluir() {
+    this.ativo = false;
+  }
 }
